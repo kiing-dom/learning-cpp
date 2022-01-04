8d2eb15d1a7797970281e84d05f9a51608431276
Dominion Gbadamosi <74149150+kiing-dom@users.noreply.github.com>
2022-01-04 13:53:13 +0000

    Create cin-calculator.cpp

---
+++ projects/cin-calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout << "Type a number: ";
+    cin >> x;
+
+    cout << "Type another number: ";
+    cin >> y;
+
+    int sum = x + y;
+    cout << "Sum is: " << sum;
+}
